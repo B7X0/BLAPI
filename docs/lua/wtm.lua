6785b119076f14b40475ee169cf02fd7035e6065
B7X0 <142055331+B7X0@users.noreply.github.com>
2023-08-12 17:57:28 -0300

    Create wtm.lua

---
+++ docs/lua/wtm.lua
@@ -0,0 +1,4 @@
+_G.Config = { ["Theme"] = "bit" }
+local Blx = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/B7X0/BLAPI/main/docs/NordNotifs.lua", true))();
+wait(1)
+Blx:Notify("BitLight.cc", "BitLight is attached and ready to use.", "warn", 5);
